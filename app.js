f8f4fbebe06c66626bc659dd347b0bac23921f43
H.FoxDev <intira.vinit@gmail.com>
2022-12-24 12:18:33 +0700

    Switch Case

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-// if statement
 
-let score = 68;
 
-if(score > 79){
-    document.write("A")
-} else if(score > 69) {
-    document.write("B")
-} else if(score > 59) {
-    document.write("C")
-} else if(score > 49) {
-    document.write("D")
-} else {
-    document.write("F")
+
+// Switch... case...
+month = "May"
+switch(month) {
+    case "Jan": console.log("January");
+        break;
+    case "Feb": console.log("Febuary");
+        break;
+    case "Mac": console.log("Mach");
+        break;
+    default: break;
 }
-
-// Ternary Operator
-gread = score > 70 ? "PASS" : "NOT PASS"
-console.log(gread)
