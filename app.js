6b1d66f4df01b65b90ecdff6078061c932a693fc
H.FoxDev <intira.vinit@gmail.com>
2022-12-27 13:58:07 +0700

    Array For Loop

--- app.js
+++ app.js
@@ -1,18 +1,7 @@
-// array
+// array ForLoop
 
 let colors = ['Red', 'White', 'Blue', 'Yellow']
-let fruits = ['มะม่วง', 'แอปเปิ้ล', 'กล้วย', 'ทุเรียน']
 
-console.log("Count :", colors.length)
-
-console.log("Before :", colors)
-console.log("After :", colors.sort())
-
-console.log("First :", colors[0])
-console.log("End :", colors[colors.length - 1])
-
-console.log("Sort :", fruits.sort())
-console.log("Reverse :", fruits.reverse())
-
-fruits.push("ส้ม")
-console.log("Add new member :", fruits)
+for(let i=0; i < colors.length; i++) {
+    console.log(i, colors[i])
+}
