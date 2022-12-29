baf10f782ca804c95456903090a6b45469b90be5
H.FoxDev <intira.vinit@gmail.com>
2022-12-29 11:46:17 +0700

    DOM Nodes Remove and Replace
    - removeChild
    - repleacChild

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-// DOM Nodes
-
-const textAll = document.querySelectorAll("p")
-console.log(textAll);
-
-let msg = textAll[1].innerHTML
-console.log(msg);
+// DOM Nodes create, remove, append, replace
 
 const menu = document.getElementById('menu');
 let count = 1;
 
-function addItem(){
-    const item = document.createElement('li');
-    item.innerText = "Coffee " + count;
-    menu.appendChild(item)
-    count++
+function addItem(){ 
+    const item_list = document.createElement('li');   
+    item_list.innerText = "Coffee " + count;
+    item_list.id = "coffee_" + count++
+    menu.appendChild(item_list)
+};
+
+function delItem(){
+    item = document.getElementById("coffee_" + --count)
+    menu.removeChild(item)
+};
+
+function replaceItem(){
+    item = document.getElementById("coffee_" + --count)
+
+    item_b = document.createElement('li')
+    item_b.innerText = "Bekery_" + count
+    item_b.id = "bekery_" + count
+    
+    menu.replaceChild(item_b, item)
 };
