7d1477c2fe2fe174ba384dbf2ac96565cda1437e
H.FoxDev <intira.vinit@gmail.com>
2023-01-03 13:55:39 +0700

    DOM Event onload

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-// DOM CSS Add & Remove Class
+// DOM Event
 
-const box = document.getElementById('box')
-let o_status;
-
-function setDarkMode(){
-    box.classList.add('darkMode')
-}
-
-function removeDarkMode(){
-    box.classList.remove('darkMode')
-}
-
-function switchMode(){
-    box.classList.toggle("darkMode")
-    o_status = box.classList.contains("darkMode")
-    console.log(o_status);
-}
+ function welcome(){
+    alert("Welcome!!!");
+ };
